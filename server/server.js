22764645e75028305c903647f2893c529c807b54
aaronmassey45 <aaronmassey45@gmail.com>
2017-11-10 18:17:00 -0600

    Added get by id todo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
@@ -9,6 +10,7 @@ let app = express();
 
 app.use(bodyParser.json());
 
+//POST
 app.post('/todos', (req, res) => {
   let todo = new Todo({
     text: req.body.text
@@ -21,11 +23,27 @@ app.post('/todos', (req, res) => {
   });
 });
 
+//GET ALL
 app.get('/todos', (req, res) => {
   Todo.find().then(todos => {
     res.send({ todos })
   }, err => {
     res.status(400).send(err);
+  });
+});
+
+//GET ONE
+app.get('/todos/:id', (req, res) => {
+  let { id } = req.params;
+
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+
+  Todo.findById(id).then(todo => {
+    if (!todo) return res.status(404).send();
+
+    res.send({ todo });
+  }).catch(err => {
+    res.status(400).send();
   });
 });
 
