2d6a181a646f5b5150c9db4cf06f403731dd1b16
aaronmassey45 <aaronmassey45@gmail.com>
2017-11-18 12:42:09 -0600

    Added POST users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,35 @@
+const { SHA256 } = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+let data = {
+  id: 8
+};
+
+let token = jwt.sign(data, 'swolepaps');
+console.log(token);
+
+let decoded = jwt.verify(token, 'swolepaps');
+console.log('decoded', decoded);
+
+// let message = 'I am swolepapi';
+// let hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+//
+// let data = {
+//   id: 4
+// };
+//
+// let token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+//
+// //ATTACK WHERE DATA IS UPDATED
+// // token.data.id = 5;
+// // token.hash = JSON.stringify(token.data).toString();
+//
+// let resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+//
+// console.log(resultHash === token.hash ? 'Data not changed' : 'Data was changed. Do not trust!');
--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,59 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-let User = mongoose.model('User', {
+//using schema allows you to to add methods
+let UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
+
+//create a schema method
+UserSchema.methods.generateAuthToken = function () {
+  let user = this;
+  let access = 'auth';
+  let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+UserSchema.methods.toJSON = function () {
+  let user = this;
+  let userObj = user.toObject();
+
+  return _.pick(userObj, ['_id', 'email']);
+};
+
+//create a User model
+let User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
